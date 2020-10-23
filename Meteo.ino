88c85b93f52491c39f5e72070cf03b990c0ee6b7
Ovidiu D. Nitan <nitanovidiu@gmail.com>
2020-10-23 14:54:39 +0300

    Updated radio init

--- Meteo.ino
+++ Meteo.ino
@@ -1,87 +1,50 @@
-#include <ESP8266WiFi.h>
-#include <Ticker.h>
-#include <FS.h>
+// Arduino Pro or Pro Mini
+// ATmega328P 3.3V 8MHz
+
+#include <RF24.h>
 #include <DHT.h>
 #include <LiFuelGauge.h>
-#include <PubSubClient.h>
-#include <ArduinoJson.h>
+#include <LowPower.h>
+#include <pb_encode.h>
+#include <pb_decode.h>
+#include <limits.h>
+#include "DataPacket.pb.h"
 
-const long MAX_LOOP_TIME_MS = 60000;
+#define TEMP_CHANNEL "WHTM"
+#define HUM_CHANNEL "WHHM"
+#define BATT_CHANNEL "WHBT"
+#define SLEEP_DURATION 600 // 600s = 30 mins
+uint8_t radioAddress[9] = "Gateway1";
 
-// Configuration
-String WIFI_SSID;
-String WIFI_PASSWORD;
-String mqtt_server;
-int mqtt_port = 0;
-
+RF24 radio(8, 10);
 LiFuelGauge gauge(MAX17043);
-DHT dht(D4, DHT22);
-WiFiClient espClient;
-PubSubClient mqttClient;
-Ticker sleepTicker;
+DHT dht(4, DHT22);
+
+void(*resetFunc) (void) = 0; //declare reset function @ address 0
 
 void setup() {
-  // put your setup code here, to run once:
-  sleepTicker.once_ms(MAX_LOOP_TIME_MS, &goToSleep);  // 60 sec timeout
-  
   Serial.begin(9600);
-  gauge.wake();
-  dht.begin();
-  delay(2000);
-  
-  if (!loadConfiguration()) {
-    Serial.println("Failed to load config, aborting.");
-    goToSleep();
-    return;
-  }
-
-  Serial.print("Connecting to ");
-  Serial.println(WIFI_SSID);
-
-  // Connect to WiFi, if needed
-  if (WiFi.SSID() != WIFI_SSID) {
-    WiFi.begin(WIFI_SSID.c_str(), WIFI_PASSWORD.c_str());
-    WiFi.persistent(true);
-    WiFi.setAutoConnect(true);
-    WiFi.setAutoReconnect(true);
-  }
-
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-  
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  //espClient.setSync(true);
-  mqttClient.setClient(espClient);
-  mqttClient.setServer(mqtt_server.c_str(), mqtt_port);
-  Serial.print("Attempting MQTT connection to "); Serial.print(mqtt_server); Serial.print(":"); Serial.print(mqtt_port), Serial.print(" ");
-  while (!mqttClient.connected()) {
-    // Attempt to connect
-    if (mqttClient.connect("Meteo")) {
-      Serial.println(" connected");
-    } else {
-      Serial.print(".");
-      delay(500);
-    }
-  }
-
+  pinMode(2, OUTPUT);
+  pinMode(6, OUTPUT);
+  Serial.println("Done.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  mqttClient.loop();
+  gauge.wake();
   gauge.quickStart();
+  
+  if (!wakeUp()) {
+    Serial.println("Failed to wake up, aborting.");
+    goToSleep();
+    return;
+  }
+  delay(2000);
 
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
   float realFeel = dht.computeHeatIndex(temperature, humidity, false);
   float battery = gauge.getSOC();
-  
+
   if (isnan(humidity) || isnan(temperature)) {
     Serial.println("Failed to read sensor data.");
   } else {
@@ -90,81 +53,64 @@ void loop() {
     Serial.print(temperature);
     Serial.print(" HUM: ");
     Serial.print(humidity);
-    if (battery > 0.0) {
-      Serial.print(" BATT: ");
-      Serial.print(battery);
-    }
+    Serial.print(" BATT: ");
+    Serial.print(battery);
     Serial.println("");
-  
-    Serial.println("Publishing values over MQTT");
-    mqttClient.publish("meteo/humidity", String(humidity, 1).c_str(), true);
-    mqttClient.publish("meteo/temperature", String(temperature, 1).c_str(), true);
-    mqttClient.publish("meteo/real_feel", String(realFeel, 1).c_str(), true);
+
+    Serial.println("Publishing value over the air");
+    // Temperature
+    broadcastData(TEMP_CHANNEL, temperature);
+    // Humidity
+    broadcastData(HUM_CHANNEL, humidity);
+    // Battery
     if (battery > 0.0) {  // Check we still have a battery, lol
-      mqttClient.publish("meteo/battery", String(battery, 1).c_str(), true);
+      broadcastData(BATT_CHANNEL, battery);
     }
-    
-    Serial.println("Done, preparing to sleep");
   }
-  
-  // Disconnect from the MQTT server and WiFi
-  mqttClient.disconnect();
-  delay(5000);
-  WiFi.disconnect();
-  while (WiFi.status() == WL_CONNECTED) { delay(500); }
+
   goToSleep();
 }
 
-bool loadConfiguration() {
-  Serial.println("Reading config data...");
-  SPIFFS.begin();
-  // Do some checks...
-  if (!SPIFFS.exists("/config.json")) {
-    Serial.println("CONFIGURATION FILE DOES NOT EXIST, ABORTING.");
-    SPIFFS.end();
-    return false;
-  }
+bool wakeUp() {
+  digitalWrite(2, HIGH);
+  digitalWrite(6, HIGH);
   
-  File configFile = SPIFFS.open("/config.json", "r");
-  if (!configFile) {
-    Serial.println("FAILED TO OPEN CONFIGURATION FILE, ABORTING.");
-    SPIFFS.end();
+  dht.begin();
+  Serial.println("Initializing radio...");
+  if (!radio.begin()) {
+    Serial.println("FAIL");
     return false;
   }
-
-  // Read file's contents
-  size_t size = configFile.size();
-  std::unique_ptr<char[]> buf(new char[size]);
-  configFile.readBytes(buf.get(), size);
-
-  // Parse file
-  DynamicJsonDocument doc;
-  DeserializationError jsonError = deserializeJson(doc, buf.get());
-  if (jsonError) {
-    Serial.println("FAILED TO PARSE CONFIGURATION FILE, ABORTING.");
-    SPIFFS.end();
-    return false;
-  }
-
-  // Load config values from files
-  JsonObject json = doc.as<JsonObject>();
-  WIFI_SSID = json["wifi_name"].as<String>();
-  WIFI_PASSWORD = json["wifi_pass"].as<String>();
-  mqtt_server = json["mqtt_server"].as<String>();
-  mqtt_port = json["mqtt_port"];
-  SPIFFS.end();
-
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setDataRate(RF24_250KBPS);
+  radio.openWritingPipe(radioAddress);
   return true;
 }
 
 void goToSleep() {
-  if (millis() >= MAX_LOOP_TIME_MS) {
-    Serial.println("Timeout occured, aborting everything and going to sleep.");
-    WiFi.disconnect();
+  Serial.println("All tasks done, going to sleep...");
+  digitalWrite(2, LOW);
+  digitalWrite(6, LOW);
+  delay(200);
+
+  for (unsigned int sleepCounter = SLEEP_DURATION/8; sleepCounter>0; sleepCounter--) {
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
   }
-  Serial.println("Going to sleep, see you in 15m.");
-  sleepTicker.detach();
-  gauge.sleep();
-  ESP.deepSleep(900e6, WAKE_RF_DEFAULT);
-  delay(5000);
+}
+
+void broadcastData(char *channel, float value) {
+  Serial.print("Sending "); Serial.print(DataPacket_size); Serial.println(" bytes to gateway...");
+  uint8_t buf[DataPacket_size];
+  uint8_t len = sizeof(buf);
+  DataPacket packet = DataPacket_init_zero;
+  packet.packet_type = DataPacket_PacketType_MEASUREMENT;
+  strncpy(packet.identifier, channel, sizeof(packet.identifier));
+  packet.value = value;
+  pb_ostream_t ostream = pb_ostream_from_buffer(buf, DataPacket_size);
+  if (pb_encode(&ostream, DataPacket_fields, &packet)) {
+    radio.writeBlocking(&buf, ostream.bytes_written, 1000);
+    radio.txStandBy(2000);
+  }
+
+  delay(500);
 }
